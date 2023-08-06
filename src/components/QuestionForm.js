3be7c79ae5bfe40423add1a82cd31a0b58db3e9e
FridaGathima <frida.gathima@student.moringaschool.com>
2023-08-06 23:37:12 +0300

    completed assignment

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -19,8 +19,28 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
+const formDataNewQue = {
+      prompt: formData.prompt,
+      answers: [
+        formData.answer1,
+        formData.answer2,
+        formData.answer3,
+        formData.answer4,
+      ],
+      correctIndex: formData.correctIndex
+
+    }    
     console.log(formData);
   }
+
+  fetch ("http://localhost:4000/questions", {
+    method: 'POST',
+    headers: {"Content-Type": "application/JSON"},
+    Body: JSON.stringify(formDataNewQue)
+  })
+
+  .then((res) => res.json())
+  .then((data) => props.addQue(data))
 
   return (
     <section>
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 function QuestionList() {
+  const [quizQuestions, setData] = useState([])
+  
+  useEffect(() => {
+    fetch ("http://localhost:4000/questions")
+    .then (response => response.json())
+    .then (data => setData(data))
+  }, [])
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <div>{/* display QuestionItem components here after fetching */}
+      {quizQuestions.map((questions) =>{
+        return <ul key={questions.id}>
+          <li>{questions.prompt}</li>
+        </ul>
+      })}</div>
     </section>
   );
 }
